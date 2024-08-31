9a483121dbf0ccecc12f8724b8cc8791a8ea679b
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-08-31 20:04:35 +0330

    Problem load img Done (in angular.json)

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8f6df871b554e6c69027d5fe43555de4c1211e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fe17cf2273770997539ca65f0f1b8e9a4d6b98")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
