7a7c495e620697941124dfcf24fde1357d5e5357
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-09-11 10:52:34 +0330

    AppUser fix and Debug

--- backend/api/Models/AppUser.cs
+++ backend/api/Models/AppUser.cs
@@ -6,25 +6,11 @@ namespace api.Models;
 [CollectionName("users")]
 public class AppUser : MongoIdentityUser<ObjectId>
 {
-    public int Schema { get; init; } = 2;
     public string? IdentifierHash { get; init; }
     public string? Name { get; init; } = string.Empty;
-    public int? PhoneNumber { get; init; }
     public string? LastName { get; init; } = string.Empty;
+    // public int? PhoneNumber { get; init; }
     public string? Gender { get; init; } = string.Empty;
     public DateOnly DateOfBirth { get; init; }
-    // public int NationalCode { get; init; }
-    // public string Class { get; init; }
-    // public List<AppRole> Roles { get; init; } = [];
-    // public string Role { get; init; } = string.Empty;
     // public List<Attendence> Attendences { get; init; } = [];
-
-    // public string? Introduction { get; init; }
-    // public string? LookingFor { get; init; }
-    // public string? Interests { get; init; }
-    // public string City { get; init; } = string.Empty;
-    // public string Country { get; init; } = string.Empty;
-    // public List<Photo> Photos { get; init; } = [];
-    // public int FollowingsCount { get; init; }
-    // public int FollowersCount { get; init; }
 }
--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2004cf8050311ceb49e584c7d4948d842b7c63bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b88286c3f8b4fea4a03da76af90ca835f169d30")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
