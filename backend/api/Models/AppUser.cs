db3506a42726374cee9bc785acabe32f7f001e3c
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-09-12 13:23:27 +0330

    Seed and AppUser update

--- backend/api/Models/AppUser.cs
+++ backend/api/Models/AppUser.cs
@@ -1,42 +1,18 @@
-// Entity
-
 using AspNetCore.Identity.MongoDbCore.Models;
 using MongoDbGenericRepository.Attributes;
 
 namespace api.Models;
-
 
 [CollectionName("users")]
 public class AppUser : MongoIdentityUser<ObjectId>
 {
     public int Schema { get; init; } = 2;
     public string? IdentifierHash { get; init; }
-    // public string? JtiValue { get; init; }
+    public string? Name { get; init; } = string.Empty;
+    public string? LastName { get; init; } = string.Empty;
+    public string? PhoneNum { get; init; }
+    public string? Gender { get; init; } = string.Empty;
     public DateOnly DateOfBirth { get; init; }
-    public string KnownAs { get; init; } = string.Empty;
-    public DateTime LastActive { get; init; }
-    public string Gender { get; init; } = string.Empty;
-    public string? Introduction { get; init; }
-    public string? LookingFor { get; init; }
-    public string? Interests { get; init; }
-    public string City { get; init; } = string.Empty;
-    public string Country { get; init; } = string.Empty;
 }
 
-// using AspNetCore.Identity.MongoDbCore.Models;
-// using MongoDbGenericRepository.Attributes;
-
-// namespace api.Models;
-
-// [CollectionName("users")]
-// public class AppUser : MongoIdentityUser<ObjectId>
-// {
-//     public int Schema { get; init; } = 2;
-//     public string? IdentifierHash { get; init; }
-//     public string? Name { get; init; } = string.Empty;
-//     public string? LastName { get; init; } = string.Empty;
-//     public string? PhoneNum { get; init; }
-//     public string? Gender { get; init; } = string.Empty;
-//     public DateOnly DateOfBirth { get; init; }
-//     // public List<Attendence> Attendences { get; init; } = [];
-// }
+// public List<Attendence> Attendences { get; init; } = [];
--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c86a93ab0be063c32be4ba03fb040476d7fd33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc60a2665c3be77e98254a93b072b5f1669dae44")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
