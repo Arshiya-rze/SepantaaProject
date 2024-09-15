b4afb9c559528c00837ebd0c0cfd2292bf3f8e0f
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-09-15 11:28:25 +0330

    remove MoId from appUser model

--- backend/api/Models/AppUser.cs
+++ backend/api/Models/AppUser.cs
@@ -2,17 +2,31 @@ using AspNetCore.Identity.MongoDbCore.Models;
 using MongoDbGenericRepository.Attributes;
 
 namespace api.Models;
-
+//New Code remove MongoIdentity
 [CollectionName("users")]
-public class AppUser : MongoIdentityUser<ObjectId>
-{
-    public int Schema { get; init; } = 2;
-    public string? IdentifierHash { get; init; }
-    public string? Name { get; init; } = string.Empty;
-    public string? LastName { get; init; } = string.Empty;
-    public string? PhoneNum { get; init; }
-    public string? Gender { get; init; } = string.Empty;
-    public DateOnly DateOfBirth { get; init; }
-}
+public record AppUser(
+    [property: BsonId, BsonRepresentation(BsonType.ObjectId)] ObjectId Id,
+    string? Email,
+    string? Name,
+    string? LastName,
+    int PhoneNumber,
+    string Password,
+    string ConfirmPassword,
+    string? Gender,
+    DateOnly? DateOfBirth
+);
+
+
+// [CollectionName("users")]
+// public class AppUser : MongoIdentityUser<ObjectId>
+// {
+//     public int Schema { get; init; } = 2;
+//     public string? IdentifierHash { get; init; }
+//     public string? Name { get; init; } = string.Empty;
+//     public string? LastName { get; init; } = string.Empty;
+//     public string? PhoneNum { get; init; }
+//     public string? Gender { get; init; } = string.Empty;
+//     public DateOnly DateOfBirth { get; init; }
+// }
 
 // public List<Attendence> Attendences { get; init; } = [];
--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5acdd609a365127a374749bb698306c0d33a8463")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efa504b8d9e6ec924e0b4bda66daa94c622d09a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
