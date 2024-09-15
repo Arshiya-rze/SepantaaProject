712c964368d75f4d6f8e03e8a6361973e76c5d1b
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-09-15 22:14:20 +0330

    AppUser model change and use MongoIdentity

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58bf0e31cfcc16032faa7030c8ca7d1aaa7f2eb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd724fd2956292e8f6908f75020f176eedca74e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
