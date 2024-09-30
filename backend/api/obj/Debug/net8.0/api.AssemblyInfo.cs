dc8f2912e531e76a2ba7d196cac36390e4341269
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-09-30 21:45:47 +0330

    add environment and service

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b078ee1070c07e9235dce73de8d1035c62b5862")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a168a8f63315933fcfbb8d4117c37e1bbbaa757b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
