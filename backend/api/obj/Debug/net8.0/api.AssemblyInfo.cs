f2884d838ea627b76215f79691617e48d471720e
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-09-30 22:07:15 +0330

    model in front end add

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a168a8f63315933fcfbb8d4117c37e1bbbaa757b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc8f2912e531e76a2ba7d196cac36390e4341269")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
