a168a8f63315933fcfbb8d4117c37e1bbbaa757b
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-09-30 12:03:17 +0330

    mat-stepper not work for image slider

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63f030c0cecb7df14b0210c8c895a1a4f147ecf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b078ee1070c07e9235dce73de8d1035c62b5862")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
