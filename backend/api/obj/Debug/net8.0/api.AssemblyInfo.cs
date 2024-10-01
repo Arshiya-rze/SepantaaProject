99d3be64027f1500ce105083315c14d61a0342ae
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-10-01 13:31:21 +0330

    add service and login work in client

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc8f2912e531e76a2ba7d196cac36390e4341269")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2884d838ea627b76215f79691617e48d471720e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
