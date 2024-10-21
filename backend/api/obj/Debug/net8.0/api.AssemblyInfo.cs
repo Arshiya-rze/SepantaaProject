3014d14b7b886b38cb17e91909ca2cab34823b21
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-10-21 16:45:09 +0330

    update node / npm / nvm but still app has error

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+355dfd1317976ee11334379f541892ed435e44ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238e2f66ebd3ef9c970a6961a62f8e3ebce96501")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
