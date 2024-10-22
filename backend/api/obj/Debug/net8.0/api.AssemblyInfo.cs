20b076fdf63e157cd52272b07e56febc2705c66d
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-10-22 05:47:54 +0330

    admin service and account seervice Done

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3014d14b7b886b38cb17e91909ca2cab34823b21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca868a7744cb8f91710ccaa688436304db9aadd")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
