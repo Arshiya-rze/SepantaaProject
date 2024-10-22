1830e8e779788076e5384edcff20219cb1788420
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-10-22 16:09:37 +0330

    bog manCompo fixed and each member have different service

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca868a7744cb8f91710ccaa688436304db9aadd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20b076fdf63e157cd52272b07e56febc2705c66d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
