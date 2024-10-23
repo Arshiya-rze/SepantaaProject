b202a1fd07be478c9f0ce3829fd0a6898d410ea3
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-10-23 19:38:12 +0330

    teacher can add-attendence Done

--- backend/api/DTOs/StudentDto.cs
+++ backend/api/DTOs/StudentDto.cs
@@ -6,7 +6,7 @@ public record AddStudentStatusDto(
     DateOnly Date,
     string AbsentOrPresent
 );
-
+  
 public class ShowStudentStatusDto
 {
     // public AppUser? UserName { get; init; }
--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46a23cc532d9ee174ecb0bef32c62873a73e67ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6edd0b594127f035543cb172d92570527d70940")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
