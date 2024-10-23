a6edd0b594127f035543cb172d92570527d70940
Arshiya Rezaie <83arshiarezaie83@gmail.com>
2024-10-23 18:21:00 +0330

    get-users-role with pageList and pagination not work

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92ca65de75117c5b7b0605a10111146eb5b689d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46a23cc532d9ee174ecb0bef32c62873a73e67ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
