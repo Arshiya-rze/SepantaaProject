fa53e9fc0614660f80d30c6e139a2a745dd207e6
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-01-07 14:50:58 +0330

    client added

--- backend/api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf0947af2670347cc4d59ec08db34745675803a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd68d16da8a72d7d7c3ffe30e2172bd8c9017450")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
