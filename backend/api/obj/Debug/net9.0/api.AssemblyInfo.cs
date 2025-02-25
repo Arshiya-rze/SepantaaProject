d38947a486db85d7f33d41ba28ecf25e2c91d764
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-02-25 18:24:05 +0330

    image-processing add helpers and interface and service Done

--- backend/api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf4a9141e2d447bf60bc7a6c7a6050f55ece537")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07d7bdedb0b79157e0aeb1f8324e86732c89ea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
